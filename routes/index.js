3c2ff85614a4b4cb0cb2f7891291215134e2d429
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2024-11-01 14:57:16 -0400

    Filtered all the various parks from the api

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,28 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 
+const API_URL = 'https://data.cityofnewyork.us/resource/vjbm-hsyr.json';
+const trail_data = ''
+
+//fetching API
+fetch(API_URL)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return res.json();
+    })
+    .then(data => {
+        trail_data = data;
+        console.log(data)
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
-
+  res.render('index', {trails: trail_data});
 });
 
 module.exports = router;
--- routes/trails.js
+++ routes/trails.js
@@ -3,28 +3,29 @@ var router = express.Router();
 const fs = require('fs');
 
 const API_URL = 'https://data.cityofnewyork.us/resource/vjbm-hsyr.json';
-
-router.get('/', (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Hello World!')
-    return;
-});
+let parks = []
 
 //fetching API
 fetch(API_URL)
     .then(res => {
-        console.log('test', res)
         if (!res.ok) {
-        throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok');
         }
         return res.json();
     })
     .then(data => {
-        console.log(data);
+        data.forEach((trail) => {   //Getting all the various parks from the api
+            if (!parks.includes(trail.park_name) && trail.park_name != undefined){
+                parks.push(trail.park_name)
+            };
+        });
     })
     .catch(error => {
         console.error('Error:', error);
     });
+
+router.get('/', (req,res,next) => {
+    res.render('trails', {trails: parks})
+});
 
 module.exports = router;
