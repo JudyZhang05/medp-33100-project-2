8db746fcd8f7a665728160fb0f50cbb680a5e619
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2024-11-02 13:28:48 -0400

    Started merging functionality into index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,48 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', {});
+// });
+
+// module.exports = router;
+
+const API_URL = 'https://data.cityofnewyork.us/resource/vjbm-hsyr.json';
+let parksName = []
+let parks = []
+
+//fetching API
+fetch(API_URL)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return res.json();
+    })
+    .then(data => {
+        data.forEach((trail) => {   //Getting all the various parks from the api
+            if (!parksName.includes(trail.park_name) && trail.park_name != undefined){
+                parksName.push(trail.park_name)
+                parks.push({
+                    'name': trail.park_name
+                    ,'amount': 1
+                });
+            }else if(parksName.includes(trail.park_name) && trail.park_name != undefined){  //counts amount of trails in each park
+                for (let i = 0; i < parks.length; i++){
+                    if(parks[i]['name'] === trail.park_name){
+                        parks[i]['amount'] += 1;
+                    }
+                }
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+router.get('/', function(req,res,next) {
+    res.render('index', {parks: parks})
 });
 
 module.exports = router;
-
