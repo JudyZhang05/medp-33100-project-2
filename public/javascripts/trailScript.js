735ffa94455e3e844ade27d7fae0a1c1c1960960
ExtraPolite <imextrapolite2@gmail.com>
2024-11-06 16:05:42 -0500

    make changes to trails file and add function in javascrpit file to hide and show and hide map and bar chart

--- public/javascripts/trailScript.js
+++ public/javascripts/trailScript.js
@@ -1,13 +1,12 @@
 //URGENT -> GRAPH (plotly)
 
 //Obtain selected park
-let selectedPark = ''
-if(typeof(localStorage) !== 'undefined'){
-    selectedPark = localStorage.getItem('park')
-}else{
-    selectedPark = 'Goodhue Park'
+let selectedPark = '';
+if (typeof localStorage !== 'undefined') {
+  selectedPark = localStorage.getItem('park');
+} else {
+  selectedPark = 'Goodhue Park';
 }
-
 
 const titleEl = document.querySelector('title');
 const parkTitle = document.querySelector('.park-information h1');
@@ -15,43 +14,62 @@ const parkTitle = document.querySelector('.park-information h1');
 const showDifficulty = document.querySelector('.show-difficulty');
 // const showBarGraph = document.querySelector('.difficulty-bar-graph')
 
-function countTrails(){
-    let numberOfTrails = 0;
-    
+function countTrails() {
+  let numberOfTrails = 0;
 }
 
-function setUp(){
-    titleEl.innerHTML = selectedPark;
-    parkTitle.innerHTML = selectedPark;
+function setUp() {
+  titleEl.innerHTML = selectedPark;
+  parkTitle.innerHTML = selectedPark;
 }
-setUp()
+setUp();
 
-function plotBarGraph(){
-    let parkIndex = parkName.indexOf(selectedPark);
-    console.log(parkTrails[parkIndex])
-    let trace1 = {
-        x: ['Level 1', 'Level 2', 'Level 3', 'Level 4'],
-        y: parkTrails[parkIndex],
-        marker:{
-          color: ['#067600', '#FFD015', '#FF8341', '#FF3F3F']
-        },
-        type: 'bar'
-      };
-      
-    let data = [trace1];
-    
-    let layout = {
-        title: `${selectedPark}'s Trail Difficulty`,
-        xaxis: {title: 'Difficulty'},
-        yaxis: {title: 'Quantity'},
-        paper_bgcolor: '#FEFAE0',
-        plot_bgcolor: '#FEFAE0',
-        // width: 700,  
-        // height: 700,  
-        // margin: {l:20, r:20, t:100, b:20},
-        barcornerradius: 15,
-    };
+function plotBarGraph() {
+  let parkIndex = parkName.indexOf(selectedPark);
+  console.log(parkTrails[parkIndex]);
+  let trace1 = {
+    x: ['Level 1', 'Level 2', 'Level 3', 'Level 4'],
+    y: parkTrails[parkIndex],
+    marker: {
+      color: ['#067600', '#FFD015', '#FF8341', '#FF3F3F'],
+    },
+    type: 'bar',
+  };
 
-    Plotly.newPlot('bar-graph', data, layout, {responsive: true});
+  let data = [trace1];
+
+  let layout = {
+    title: `${selectedPark}'s Trail Difficulty`,
+    xaxis: { title: 'Difficulty' },
+    yaxis: { title: 'Quantity' },
+    paper_bgcolor: '#FEFAE0',
+    plot_bgcolor: '#FEFAE0',
+    // width: 700,
+    // height: 700,
+    // margin: {l:20, r:20, t:100, b:20},
+    barcornerradius: 15,
+  };
+
+  Plotly.newPlot('bar-graph', data, layout, { responsive: true });
 }
-plotBarGraph()
+
+// hide and show map and bar graph
+function barGraphMap() {
+  const allButtons = document.querySelectorAll('.show-button');
+  const allSections = document.querySelectorAll('.sections');
+
+  allButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      allSections.forEach((section) => {
+        section.style.display = 'none';
+      });
+
+      if (allSections[index].id === button.id) {
+        allSections[index].style.display = 'block';
+      }
+    });
+  });
+}
+
+barGraphMap();
+plotBarGraph();
