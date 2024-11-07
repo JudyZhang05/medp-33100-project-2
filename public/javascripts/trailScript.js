8f0e01bd5f0249d3ca5589095671f82a599bf3ae
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2024-11-06 19:02:57 -0500

    Update bargraph style and default bar graph display

--- public/javascripts/trailScript.js
+++ public/javascripts/trailScript.js
@@ -10,6 +10,9 @@ const titleEl = document.querySelector('title');
 const parkTitle = document.querySelector('.park-information h1');
 const showDifficulty = document.querySelector('.show-difficulty');
 let otherParks = document.querySelectorAll('.change-content div');
+
+// const bargraph = document.querySelector('#bar-graph');
+
 let newParkName = []
 let parkIndex = -1;
 
@@ -43,33 +46,6 @@ function decodeHtml() { //ridding parkName list of HTML entities returns new lis
   }
 }
 
-function plotBarGraph() {
-  let parkIndex = parkName.indexOf(selectedPark);
-  console.log(parkTrails[parkIndex]);
-  let trace1 = {
-    x: ['Level 1', 'Level 2', 'Level 3', 'Level 4'],
-    y: parkTrails[parkIndex],
-    marker: {
-      color: ['#067600', '#FFD015', '#FF8341', '#FF3F3F'],
-    },
-    type: 'bar',
-  };
-
-  let data = [trace1];
-
-  let layout = {
-    title: `${selectedPark}'s Trail Difficulty`,
-    xaxis: { title: 'Difficulty' },
-    yaxis: { title: 'Quantity' },
-    paper_bgcolor: '#FEFAE0',
-    plot_bgcolor: '#FEFAE0',
-    barcornerradius: 15,
-  };
-
-  Plotly.newPlot('bar-graph', data, layout, { responsive: true });
-}
-
-
 function plotBarGraph(){    //display bar graph
     let trace1 = {
         x: ['Level 1', 'Level 2', 'Level 3', 'Level 4'],
@@ -90,13 +66,11 @@ function plotBarGraph(){    //display bar graph
         plot_bgcolor: '#FEFAE0',
         barcornerradius: 15,
     };
-
+    
     Plotly.newPlot('bar-graph', data, layout, {responsive: true});
 
     countTrails()
 }
-
-console.log('hi')
 
 otherParks.forEach((park) => { //right navigation bar
   park.addEventListener('click', (event) => {
@@ -107,21 +81,24 @@ otherParks.forEach((park) => { //right navigation bar
 
 // hide and show map and bar graph
 // function barGraphMap() {
-//   const allButtons = document.querySelectorAll('.show-button');
-//   const allSections = document.querySelectorAll('.sections');
-
-//   allButtons.forEach((button, index) => {
-//     button.addEventListener('click', () => {
-//       allSections.forEach((section) => {
-//         section.style.display = 'none';
-//       });
-
-//       if (allSections[index].id === button.id) {
-//         allSections[index].style.display = 'block';
-//       }
-//     });
-//   });
 // }
 
-// barGraphMap();
 // plotBarGraph();
+
+
+const allButtons = document.querySelectorAll('.show-button');
+const allSections = document.querySelectorAll('.sections');
+allSections[0].style.display = 'block';
+
+allButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    allSections.forEach((section) => {
+      section.style.display = 'none';
+    });
+    if (allSections[index].id === button.id) {
+      allSections[index].style.display = 'block';
+    }
+  });
+});
+
+// barGraphMap();
